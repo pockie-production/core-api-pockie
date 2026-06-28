469aae9b8a018b993f0ff00bdcd9fc68a7ac8a40
KhoaMinh <93652831+KhoaMinhPMK@users.noreply.github.com>
2026-06-28 14:19:08 +0700

    feat: apply phase 1 schema and docker

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
+  // Validation
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  // Swagger Setup
+  const config = new DocumentBuilder()
+    .setTitle('Pockie Core API')
+    .setDescription('The core API for Pockie Platform')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  // CORS
+  app.enableCors();
+
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
